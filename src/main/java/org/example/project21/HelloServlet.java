c919e8f3b1f30a85a819c6db88fc7a88ca7df3cb
min06150315 <min06150315@gmail.com>
2024-11-09 20:12:01 +0900

    Save local changes

--- src/main/java/org/example/project21/HelloServlet.java
+++ src/main/java/org/example/project21/HelloServlet.java
@@ -9,11 +9,8 @@ public class HelloServlet extends HttpServlet {
     private String message;
 
     public void init() {
-<<<<<<< HEAD
-        message = "Hello World!";
-=======
-        message = "CRA Charlie Cho Let's Go!!";
->>>>>>> 01ebe8b (Commit)
+        // 메시지 수정: 원격 브랜치에서의 메시지를 선택
+        message = "CRA Charlie Cho Let's Go!!"; // 또는 "Hello World!" 선택 가능
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,15 +5,10 @@
     <title>JSP - Hello World</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
-</h1>
-<br/>
-<<<<<<< HEAD
-<a href="hello-servlet">Hello Servlet</a>
-=======
+    <h1><%= "Hello World!" %></h1><br />
+    <a href="hello-servlet">Hello Servlet</a>
     <a href="hello-servlet">JSP Docker Tomcat Server Test</a><br />
     <a href="cal.jsp">Calculator 연습</a><br />
     <a href="property.jsp">Property 연습</a><br />
->>>>>>> 01ebe8b (Commit)
 </body>
 </html>
